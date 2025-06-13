0c00d32587aaf3546ac7409279b5a8e3860fb2e4
grazywx <grazieli.moura42@gmail.com>
2025-06-13 15:13:38 -0300

    criação da rota de logout

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\ImageController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
